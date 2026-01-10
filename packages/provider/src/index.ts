c1f0202bf371b9666cc6059edf5a6ffa5fcd0307
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-10 14:10:32 +0100

    Link slates backend

---
+++ packages/provider/src/index.ts
@@ -0,0 +1,20 @@
+import { createLocallyCachedFunction } from '@lowerdeck/cache';
+import { db } from '@metorial-subspace/db';
+import { slatesProvider } from '@metorial-subspace/provider-slates';
+
+let getBackendRecord = createLocallyCachedFunction({
+  getHash: (backendOid: bigint) => backendOid.toString(),
+  ttlSeconds: 60,
+  provider: async backendOid =>
+    await db.backend.findFirstOrThrow({
+      where: { oid: backendOid }
+    })
+});
+
+export let getBackend = async ({ entity }: { entity: { backendOid: bigint } }) => {
+  let backend = await getBackendRecord(entity.backendOid);
+
+  if (backend.type == 'slates') return slatesProvider.create({ backend });
+
+  throw new Error(`Unsupported backend type: ${backend.type}`);
+};
