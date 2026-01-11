a5ad01b08976dd320507d7f90a97a06eedc15897
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-11 19:03:21 +0100

    Add search

---
+++ modules/provider-internal/src/queues/search/providerListing.ts
@@ -0,0 +1,37 @@
+import { createQueue, QueueRetryError } from '@lowerdeck/queue';
+import { db } from '@metorial-subspace/db';
+import { voyager, voyagerIndex, voyagerSource } from '@metorial-subspace/module-search';
+import { env } from '../../env';
+
+export let indexProviderListingQueue = createQueue<{ providerListingId: string }>({
+  name: 'dep/sidx/providerListing',
+  redisUrl: env.service.REDIS_URL
+});
+
+export let indexProviderListingQueueProcessor = indexProviderListingQueue.process(
+  async data => {
+    let providerListing = await db.providerListing.findUnique({
+      where: { id: data.providerListingId },
+      include: { provider: { include: { ownerTenant: true } } }
+    });
+    if (!providerListing) throw new QueueRetryError();
+
+    await voyager.record.index({
+      sourceId: voyagerSource.id,
+      indexId: voyagerIndex.providerListing.id,
+
+      documentId: providerListing.id,
+      tenantIds: providerListing.provider.ownerTenant
+        ? [providerListing.provider.ownerTenant.id]
+        : [],
+
+      fields: { providerId: providerListing.provider.id, configId: providerListing.id },
+      body: {
+        name: providerListing.name,
+        readme: providerListing.readme,
+        description: providerListing.description,
+        providerName: providerListing.provider.name
+      }
+    });
+  }
+);
---
+++ modules/provider-internal/src/queues/search/publisher.ts
@@ -0,0 +1,31 @@
+import { createQueue, QueueRetryError } from '@lowerdeck/queue';
+import { db } from '@metorial-subspace/db';
+import { voyager, voyagerIndex, voyagerSource } from '@metorial-subspace/module-search';
+import { env } from '../../env';
+
+export let indexPublisherQueue = createQueue<{ publisherId: string }>({
+  name: 'dep/sidx/publisher',
+  redisUrl: env.service.REDIS_URL
+});
+
+export let indexPublisherQueueProcessor = indexPublisherQueue.process(async data => {
+  let publisher = await db.publisher.findUnique({
+    where: { id: data.publisherId },
+    include: { tenant: true }
+  });
+  if (!publisher) throw new QueueRetryError();
+
+  await voyager.record.index({
+    sourceId: voyagerSource.id,
+    indexId: voyagerIndex.publisher.id,
+
+    documentId: publisher.id,
+    tenantIds: publisher.tenant ? [publisher.tenant.id] : [],
+
+    fields: { publisherId: publisher.id, configId: publisher.id },
+    body: {
+      name: publisher.name,
+      description: publisher.description
+    }
+  });
+});
