8e728f3afca67ca15cecbe2cf11cb29c0bc64bf1
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-11 19:03:58 +0100

    Add deployment services

---
+++ modules/provider-internal/src/services/providerDeployment.ts
@@ -0,0 +1,95 @@
+import { delay } from '@lowerdeck/delay';
+import { Service } from '@lowerdeck/service';
+import {
+  db,
+  Provider,
+  ProviderDeployment,
+  ProviderVariant,
+  ProviderVersion
+} from '@metorial-subspace/db';
+
+class providerDeploymentInternalServiceImpl {
+  async getCurrentVersion(d: {
+    deployment: ProviderDeployment;
+    provider: Provider & {
+      defaultVariant?:
+        | (ProviderVariant & {
+            currentVersion?: ProviderVersion | null;
+          })
+        | null;
+    };
+  }) {
+    return this.getCurrentVersionOptional({
+      deployment: d.deployment,
+      provider: d.provider
+    });
+  }
+
+  async getCurrentVersionOptional(d: {
+    deployment?: ProviderDeployment;
+    provider: Provider & {
+      defaultVariant?:
+        | (ProviderVariant & {
+            currentVersion?: ProviderVersion | null;
+          })
+        | null;
+    };
+  }) {
+    let inner = await this.getCurrentVersionInner(d);
+
+    if (inner.specificationDiscoveryStatus == 'discovering') {
+      for (let i = 0; i < 50; i++) {
+        await delay(100);
+        inner = await this.getCurrentVersionInner(d);
+        if (inner.specificationDiscoveryStatus != 'discovering') break;
+      }
+    }
+
+    return inner;
+  }
+
+  private async getCurrentVersionInner(d: {
+    deployment?: ProviderDeployment;
+    provider: Provider & {
+      defaultVariant?:
+        | (ProviderVariant & {
+            currentVersion?: ProviderVersion | null;
+          })
+        | null;
+    };
+  }) {
+    if (!d.provider.defaultVariantOid) throw new Error('Provider has no default variant oid');
+
+    if (d.deployment?.lockedVersionOid) {
+      return await db.providerVersion.findFirstOrThrow({
+        where: {
+          oid: d.deployment.lockedVersionOid,
+          providerOid: d.provider.oid
+        }
+      });
+    }
+
+    let defaultVariant =
+      d.provider.defaultVariant ??
+      (await db.providerVariant.findFirstOrThrow({
+        where: { oid: d.provider.defaultVariantOid }
+      }));
+
+    if (!defaultVariant.currentVersionOid) {
+      throw new Error('Provider variant has no current version oid');
+    }
+
+    let currentVersion =
+      d.provider.defaultVariant?.currentVersion ??
+      (await db.providerVersion.findFirstOrThrow({
+        where: { oid: defaultVariant.currentVersionOid }
+      }));
+
+    return currentVersion;
+  }
+}
+
+export let providerDeploymentInternalService = Service.create(
+  'providerDeploymentInternalService',
+  () => new providerDeploymentInternalServiceImpl()
+).build();
---
+++ modules/provider-internal/src/services/providerDeploymentConfigPair.ts
@@ -0,0 +1,164 @@
+import { delay } from '@lowerdeck/delay';
+import { Service } from '@lowerdeck/service';
+import {
+  addAfterTransactionHook,
+  db,
+  getId,
+  ProviderConfig,
+  ProviderDeployment,
+  ProviderVersion,
+  withTransaction
+} from '@metorial-subspace/db';
+import {
+  providerDeploymentConfigPairCreatedQueue,
+  providerDeploymentConfigPairVersionCreatedQueue
+} from '../queues/lifecycle/deploymentConfigPair';
+
+class providerDeploymentConfigPairInternalServiceImpl {
+  private async upsertDeploymentConfigPairWithoutCreatingVersion(d: {
+    deployment: ProviderDeployment;
+    config: ProviderConfig;
+    version?: ProviderVersion;
+  }) {
+    return withTransaction(async db => {
+      let existing = await db.providerDeploymentConfigPair.findUnique({
+        where: {
+          providerConfigOid_providerDeploymentOid: {
+            providerDeploymentOid: d.deployment.oid,
+            providerConfigOid: d.config.oid
+          }
+        },
+        include: {
+          versions: d.version ? { where: { versionOid: d.version.oid } } : false
+        }
+      });
+      if (existing) {
+        return {
+          pair: existing,
+          version: existing.versions[0],
+          created: false
+        };
+      }
+
+      let newId = getId('providerDeploymentConfigPair');
+      let pair = await db.providerDeploymentConfigPair.upsert({
+        where: {
+          providerConfigOid_providerDeploymentOid: {
+            providerDeploymentOid: d.deployment.oid,
+            providerConfigOid: d.config.oid
+          }
+        },
+        create: {
+          ...newId,
+          providerDeploymentOid: d.deployment.oid,
+          providerConfigOid: d.config.oid,
+          tenantOid: d.deployment.tenantOid
+        },
+        update: {},
+        include: {
+          versions: d.version ? { where: { versionOid: d.version.oid } } : false
+        }
+      });
+
+      let created = pair.id == newId.id;
+
+      if (created) {
+        await addAfterTransactionHook(async () =>
+          providerDeploymentConfigPairCreatedQueue.add({
+            providerDeploymentConfigPairId: pair.id
+          })
+        );
+      }
+
+      return {
+        pair,
+        created,
+        version: pair.versions[0]
+      };
+    });
+  }
+
+  async upsertDeploymentConfigPair(d: {
+    deployment: ProviderDeployment;
+    config: ProviderConfig;
+    version?: ProviderVersion;
+  }) {
+    return withTransaction(async db => {
+      let res = await this.upsertDeploymentConfigPairWithoutCreatingVersion(d);
+
+      if (!d.version) {
+        return {
+          pair: res.pair,
+          version: undefined
+        };
+      }
+
+      if (res.version) {
+        return {
+          pair: res.pair,
+          version: res.version
+        };
+      }
+
+      let newId = getId('providerDeploymentConfigPairProviderVersion');
+      let version = await db.providerDeploymentConfigPairProviderVersion.upsert({
+        where: {
+          pairOid_versionOid: {
+            pairOid: res.pair.oid,
+            versionOid: d.version.oid
+          }
+        },
+        create: {
+          ...newId,
+          pairOid: res.pair.oid,
+          versionOid: d.version.oid,
+          specificationDiscoveryStatus: 'discovering'
+        },
+        update: {}
+      });
+
+      if (version.id == newId.id) {
+        await addAfterTransactionHook(async () =>
+          providerDeploymentConfigPairVersionCreatedQueue.add({
+            providerDeploymentConfigPairVersionId: version.id
+          })
+        );
+      }
+
+      return {
+        pair: res.pair,
+        version
+      };
+    });
+  }
+
+  async useDeploymentConfigPair(d: {
+    deployment: ProviderDeployment;
+    config: ProviderConfig;
+    version: ProviderVersion;
+  }) {
+    let res = await this.upsertDeploymentConfigPair(d);
+
+    // This should never happen
+    if (!res.version) {
+      throw new Error('Failed to create deployment config pair version');
+    }
+
+    if (res.version.specificationDiscoveryStatus == 'discovering') {
+      for (let i = 0; i < 50; i++) {
+        await delay(100);
+        res.version = await db.providerDeploymentConfigPairProviderVersion.findFirstOrThrow({
+          where: { oid: res.version.oid }
+        });
+        if (res.version.specificationDiscoveryStatus != 'discovering') break;
+      }
+    }
+
+    return res;
+  }
+}
+
+export let providerDeploymentConfigPairInternalService = Service.create(
+  'providerDeploymentConfigPairInternalService',
+  () => new providerDeploymentConfigPairInternalServiceImpl()
+).build();
