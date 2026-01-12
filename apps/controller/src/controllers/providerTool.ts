17335684ed5d2beed67e1b477c106e6ba638b6e2
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-12 19:26:01 +0100

    Add spec endpoints

---
+++ apps/controller/src/controllers/providerTool.ts
@@ -0,0 +1,44 @@
+import { Paginator } from '@lowerdeck/pagination';
+import { v } from '@lowerdeck/validation';
+import { providerToolPresenter } from '@metorial-subspace/db';
+import { providerToolService } from '@metorial-subspace/module-catalog';
+import { app } from './_app';
+import { tenantApp } from './tenant';
+
+export let providerToolApp = tenantApp.use(async ctx => {
+  let providerToolId = ctx.body.providerToolId;
+  if (!providerToolId) throw new Error('ProviderTool ID is required');
+
+  let providerTool = await providerToolService.getProviderToolById({
+    providerToolId,
+    tenant: ctx.tenant,
+    solution: ctx.solution
+  });
+
+  return { providerTool };
+});
+
+export let providerToolController = app.controller({
+  list: tenantApp
+    .handler()
+    .input(Paginator.validate(v.object({})))
+    .do(async ctx => {
+      let paginator = await providerToolService.listProviderTools({
+        tenant: ctx.tenant,
+        solution: ctx.solution
+      });
+
+      let list = await paginator.run(ctx.input);
+
+      return Paginator.presentLight(list, providerToolPresenter);
+    }),
+
+  get: providerToolApp
+    .handler()
+    .input(
+      v.object({
+        providerToolId: v.string()
+      })
+    )
+    .do(async ctx => providerToolPresenter(ctx.providerTool))
+});
