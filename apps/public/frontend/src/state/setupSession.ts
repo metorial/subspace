b66b5a268b978a04784ed932954e87ba67568022
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-14 13:20:09 +0100

    Improve preload

---
+++ apps/public/frontend/src/state/setupSession.ts
@@ -0,0 +1,42 @@
+import { ErrorData, ServiceError } from '@lowerdeck/error';
+import { createLoader } from '@metorial-io/data-hooks';
+import { htmlDecode } from '../../../src/lib/htmlEncode';
+import { client } from './client';
+
+let PRELOAD = JSON.parse(htmlDecode(document.querySelector('#preload-data')?.textContent!)) as
+  | {
+      type: 'error';
+      error: ErrorData<string, number>;
+    }
+  | {
+      type: 'data';
+      data: Awaited<ReturnType<typeof client.setupSession.get>>;
+      input: { sessionId: string; clientSecret: string };
+    };
+
+export let setupSessionState = createLoader({
+  name: 'setupSession',
+  fetch: (d: { sessionId: string; clientSecret: string }) =>
+    client.setupSession.get({
+      sessionId: d.sessionId,
+      clientSecret: d.clientSecret
+    }),
+  mutators: {}
+});
+
+export let useSetupSession = () => {
+  let data = setupSessionState.use(
+    PRELOAD.type === 'error'
+      ? null
+      : {
+          sessionId: PRELOAD.data.session.id,
+          clientSecret: PRELOAD.input.clientSecret
+        }
+  );
+
+  if (PRELOAD.type === 'data' && !data.data && !data.error) data.data = PRELOAD.data;
+  if (PRELOAD.type === 'error' && !data.data && !data.error)
+    data.error = ServiceError.fromResponse(PRELOAD.error);
+
+  return data;
+};
