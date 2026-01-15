1ddedd9116b66f896e322ddde79d27622641ce54
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-01-15 17:01:11 +0100

    Add wire protocol sender

---
+++ modules/connection/src/presenter/providerTool.ts
@@ -0,0 +1,28 @@
+import { ProviderTool, SessionProvider, SessionProviderInstance } from '@metorial-subspace/db';
+
+export let providerToolPresenter = (
+  providerTool: ProviderTool & {
+    sessionProvider: SessionProvider;
+    sessionProviderInstance: SessionProviderInstance;
+  }
+) => ({
+  object: 'provider.capabilities.tool',
+
+  id: providerTool.id,
+  key: `${providerTool.sessionProvider.tag}_${providerTool.key}`,
+
+  sessionProvider: providerTool.sessionProvider.id,
+
+  name: providerTool.name,
+  description: providerTool.description,
+
+  capabilities: providerTool.value.capabilities,
+  constraints: providerTool.value.constraints,
+  inputJsonSchema: providerTool.value.inputJsonSchema,
+  instructions: providerTool.value.instructions,
+  outputJsonSchema: providerTool.value.outputJsonSchema,
+  tags: providerTool.value.tags,
+
+  createdAt: providerTool.createdAt,
+  updatedAt: providerTool.updatedAt
+});
---
+++ modules/connection/src/presenter/sessionMessage.ts
@@ -0,0 +1,13 @@
+import { SessionMessage } from '@metorial-subspace/db';
+
+export let sessionMessagePresenter = (sessionMessage: SessionMessage) => ({
+  object: 'session.message',
+
+  id: sessionMessage.id,
+  status: sessionMessage.status,
+  type: sessionMessage.type,
+
+  toolKey: sessionMessage.toolKey,
+
+  createdAt: sessionMessage.createdAt
+});
