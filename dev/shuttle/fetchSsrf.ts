6341d4fdecf39802bb3053f90d57c3e49ed106d2
Ivan <vnxdeveloper@gmail.com>
2026-02-10 16:26:09 +0100

    Enable env vars

--- dev/shuttle/fetchSsrf.ts
+++
@@ -1,9 +0,0 @@
-// Dev override: allows private/local IPs for local development.
-
-type SafeFetchOptions = RequestInit & {
-  maxRedirects?: number;
-};
-
-export let safeFetch = async (input: string, options: SafeFetchOptions = {}) => {
-  return fetch(input, options);
-};
